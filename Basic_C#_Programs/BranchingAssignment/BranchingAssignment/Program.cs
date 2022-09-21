94589c14806c90567473c4b74765f1a797da7c37
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-09-21 08:43:50 -0700

    Added comments to program for Branching assignment

--- Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
+++ Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
@@ -24,44 +24,51 @@ namespace BranchingAssignment
             Console.WriteLine("Please enter the weight of your package:");
             packageWeight = Convert.ToInt32(Console.ReadLine());
 
-            if (packageWeight > 50)
+            //
+            if (packageWeight >= 50)
             {
                 Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a good day.");
                 Console.ReadLine();
             }
-            else if (packageWeight <= 50)
+
+            //If packageWeight is less than 50, continue program
+            else if (packageWeight < 50)
             {
                 Console.WriteLine("What is your package width?");
                 packageWidth = Convert.ToInt32(Console.ReadLine());
 
-                if (packageWidth > 50)
+                if (packageWidth >= 50)
                 {
                     Console.WriteLine("Your package is too big to be shipped via Package Express. Have a good day.");
                     Console.ReadLine();
                 }
 
-                else if (packageWidth <= 50)
+                //If packageWeight is less than 50, continue program
+                else if (packageWidth < 50)
                 {
                     Console.WriteLine("What is your package height?");
                     packageHeight = Convert.ToInt32(Console.ReadLine());
 
-                    if (packageHeight > 50)
+                    if (packageHeight >= 50)
                     {
                         Console.WriteLine("Your package is too big to be shipped via Package Express. Have a good day.");
                         Console.ReadLine();
                     }
 
-                    else if (packageHeight <= 50)
+                    //If packageHeight is less than 50, continue program
+                    else if (packageHeight < 50)
                     {
                         Console.WriteLine("What is your package length?");
                         packageLength = Convert.ToInt32(Console.ReadLine());
 
-                        if (packageLength > 50)
+                        if (packageLength >= 50)
                         {
                             Console.WriteLine("Your package is too big to be shipped via Package Express. Have a good day.");
                             Console.ReadLine();
                         }
-                        else if (packageLength <= 50)
+
+                        //If packageLength is less than 50, continue to calculate price estimate
+                        else if (packageLength < 50)
                         {
                             packageDimensions = ((packageLength * packageWidth * packageHeight) * packageWeight) / 100;
                             float packagePrice = packageDimensions;
@@ -70,10 +77,11 @@ namespace BranchingAssignment
 
                         }
 
-
                     }
                 }
             }
+
+            //If any errors occurs, let the user know
             else
             {
                 Console.WriteLine("Something went wrong here... Please enter a valid whole number.");
