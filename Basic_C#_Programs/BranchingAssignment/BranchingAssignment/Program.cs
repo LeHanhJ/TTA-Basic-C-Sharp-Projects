ca03bee3c5c05d806fd2ccccbe1f167397a66520
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-09-21 18:23:57 -0700

    Updated Branching assignment to fit criteria

--- Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
+++ Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
@@ -36,58 +36,37 @@ namespace BranchingAssignment
             {
                 Console.WriteLine("What is your package width?");
                 packageWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is your package height?");
+                packageHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is your package length?");
+                packageLength = Convert.ToInt32(Console.ReadLine());
 
-                if (packageWidth >= 50)
+                bool packageTooBig = ((packageHeight + packageLength + packageWidth) >= 50);
+
+                if (packageTooBig == true)
                 {
-                    Console.WriteLine("Your package is too big to be shipped via Package Express. Have a good day.");
+                    Console.WriteLine("Your package is too large to be shipped via Package Express. Have a good day.");
                     Console.ReadLine();
                 }
 
-                //If packageWeight is less than 50, continue program
-                else if (packageWidth < 50)
+                //If packageLength is less than 50, continue to calculate price estimate
+                else if (packageLength < 50)
                 {
-                    Console.WriteLine("What is your package height?");
-                    packageHeight = Convert.ToInt32(Console.ReadLine());
-
-                    if (packageHeight >= 50)
-                    {
-                        Console.WriteLine("Your package is too big to be shipped via Package Express. Have a good day.");
-                        Console.ReadLine();
-                    }
-
-                    //If packageHeight is less than 50, continue program
-                    else if (packageHeight < 50)
-                    {
-                        Console.WriteLine("What is your package length?");
-                        packageLength = Convert.ToInt32(Console.ReadLine());
-
-                        if (packageLength >= 50)
-                        {
-                            Console.WriteLine("Your package is too big to be shipped via Package Express. Have a good day.");
-                            Console.ReadLine();
-                        }
-
-                        //If packageLength is less than 50, continue to calculate price estimate
-                        else if (packageLength < 50)
-                        {
-                            packageDimensions = ((packageLength * packageWidth * packageHeight) * packageWeight) / 100;
-                            float packagePrice = packageDimensions;
-                            Console.WriteLine("Your estimated total for shipping for this package is: $" + packagePrice);
-                            Console.ReadLine();
-
-                        }
-
-                    }
+                    packageDimensions = ((packageLength * packageWidth * packageHeight) * packageWeight) / 100;
+                    float packagePrice = packageDimensions;
+                    Console.WriteLine("Your estimated total for shipping for this package is: $" + packagePrice);
+                    Console.ReadLine();
                 }
-            }
 
-            //If any errors occurs, let the user know
-            else
-            {
-                Console.WriteLine("Something went wrong here... Please enter a valid whole number.");
-                Console.ReadLine();
+                //If any errors occurs, let the user know
+                else
+                {
+                    Console.WriteLine("Something went wrong here... Please enter a valid whole number.");
+                    Console.ReadLine();
+                }
             }
         }
     }
 }
+
 
--- Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -6,10 +6,25 @@ class Program
 {
    static void Main()
     {
-        string doubleQuotes = "The use of double quotes\" \"is necessary";
-        Console.WriteLine(doubleQuotes);
-        Console.ReadLine();
+        //string name = "Jesse";
+        //string quote = "The man said \" Hello, Jesse\". \n \t Hello on a tab.";
+        //string fileName = @"C:\Users\Jesse";
 
+        //bool trueOrFalse = name.EndsWith("s");
+
+        //int length = name.Length;
+
+        //name = name.ToUpper();
+
+        //Console.WriteLine(name);
+        //Console.ReadLine();
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+        sb.Append("My name is Jesse");
+
+        Console.WriteLine(sb);
+        Console.ReadLine();
     }
 }
 
