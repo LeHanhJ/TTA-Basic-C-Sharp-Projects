aff3125aaf40f505cf88afde41c6f5ecc7d2355e
LeHanhJ <101995252+LeHanhJ@users.noreply.github.com>
2022-09-22 19:36:52 -0700

    Completed String Assignment with simple concatenations and StringBuilding

---
+++ Basic_C#_Programs/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hanh";
+            string quote = "The woman said, \"Hello, " + name + ".\" \n\"I hope you are enjoying your stay here\" \n \t I just looked at her.";
+            string fileName = "C:\\Users\\Hanh \n"; //Can also use @ in front of double quotes to use on long file names
+
+            bool trueOrFalse = name.Contains("n");
+            int length = name.Length;
+
+            name = name.ToUpper();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Hanh. \n The woman scoffed and rolled her eyes. \n \t \"I know who you are, I just said your name a second ago.\"");
+
+
+            Console.WriteLine(fileName);
+            Console.WriteLine(quote);
+
+            Console.WriteLine(sb);
+
+            Console.WriteLine(trueOrFalse);
+            Console.WriteLine(length);
+            Console.WriteLine(name);
+
+            Console.ReadLine();
+        }
+    }
+}
