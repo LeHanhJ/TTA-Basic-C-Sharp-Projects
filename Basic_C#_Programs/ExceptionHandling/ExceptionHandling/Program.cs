175fc7cfc4eca8e2060f3346a567ad5866dd37f7
Hanh Le <jamiehanhle@gmail.com>
2022-09-30 11:46:12 -0700

    Completed try/catch assignment dividing integers

---
+++ Basic_C#_Programs/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+
+            }
+
+            // Different types of exception errors
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try part of try/catch
+            try
+            {
+                // 1. Created a list of integers and created a loop to iterate through each index in the list
+                List<int> integers = new List<int>() { 39, 82, 43, 54, 90, 23, 14 };
+                
+                    foreach (int num in integers)
+                    {
+                        Console.WriteLine("Please pick a number and I will divide the number on my list by yours:");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Checking if we can divide this...");
+                        // it will divide by the user inputted number...
+                        int quotient = num / number;
+                        int modulus = num % number;
+                        Console.WriteLine(num + " divided by " + number + " equals " + quotient + " with a remainder of " + modulus);
+                    }
+                // since this is in the "for" loop, it will iterate until it reaches however many indecies the list has
+                }
+
+            // catch part of try/catch
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number.");
+                return;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please do not divide by zero...");
+                return;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
