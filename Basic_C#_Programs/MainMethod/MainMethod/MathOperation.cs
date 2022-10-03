cb7d395a0594cf9d77c2b43673b9230281ccf6cc
Hanh Le <jamiehanhle@gmail.com>
2022-10-03 13:55:52 -0700

    Updated comments in project MainProgram

--- Basic_C#_Programs/MainMethod/MainMethod/MathOperation.cs
+++ Basic_C#_Programs/MainMethod/MainMethod/MathOperation.cs
@@ -10,19 +10,22 @@ namespace MainMethod
     //first put "public" in front of class to have other files in program able to access it
     public class MathOperation
     {
+        //squares a nnumber entered in variable num
         public int SquareNum(int num)
         {
             int square = num * num;
-            //remember, need "return;" to return value
+            //REMEMBER!! need "return;" to return value
             return square;
         }
 
+        //multiplies by 4%, the tax amount in Hawai'i and returns it
         public double DecimalNum(double num)
         {
             double deciNum = num * .04;
             return deciNum;
         }
 
+        //converts user input in the method rather than the main program itself.
         public int StringNum(string num2)
         {
             int numAgain = Convert.ToInt32(num2);
--- Basic_C#_Programs/MainMethod/MainMethod/Program.cs
+++ Basic_C#_Programs/MainMethod/MainMethod/Program.cs
@@ -11,15 +11,20 @@ namespace MainMethod
         static void Main(string[] args)
         {
 
+            //instanciating the MathOperation class to be able to be used in the main program
             MathOperation mathOperation = new MathOperation();
-            Console.WriteLine("Please enter a number and I will see what the number is if we square it, and multiply it by 3.25");
+
+
+            Console.WriteLine("Please enter a number and I will see what the number is if we square it, and if we were to buy something in Hawai'i at that same price, sales tax only.");
             int num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("And another number to multiply by 1000 please:");
             string num2 = Console.ReadLine();
             
+           
             Console.WriteLine(num +" multiplied by itself is " + mathOperation.SquareNum(num));
-            Console.WriteLine("In Hawai'i, you would pay $" + mathOperation.DecimalNum(num) + " more in taxes than you would in Oregon for the same price.");
+            Console.WriteLine("In Hawai'i, you would pay $" + mathOperation.DecimalNum(num) + " more in sales taxes than you would in Oregon for the same price.");
             Console.WriteLine(num2 + " multiplied by 1000 is " + mathOperation.StringNum(num2));
+
             Console.ReadLine();
         }
     }
