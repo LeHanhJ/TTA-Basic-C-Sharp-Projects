075ed059d8124fa1cdb81e4dabb59dbe8c982680
Hanh Le <jamiehanhle@gmail.com>
2022-10-03 13:52:23 -0700

    Made MainMethod assignment and performed as instructed

---
+++ Basic_C#_Programs/MainMethod/MainMethod/MathOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+
+    //first put "public" in front of class to have other files in program able to access it
+    public class MathOperation
+    {
+        public int SquareNum(int num)
+        {
+            int square = num * num;
+            //remember, need "return;" to return value
+            return square;
+        }
+
+        public double DecimalNum(double num)
+        {
+            double deciNum = num * .04;
+            return deciNum;
+        }
+
+        public int StringNum(string num2)
+        {
+            int numAgain = Convert.ToInt32(num2);
+            int product = numAgain * 1000;
+            return product;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MathOperation mathOperation = new MathOperation();
+            Console.WriteLine("Please enter a number and I will see what the number is if we square it, and multiply it by 3.25");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("And another number to multiply by 1000 please:");
+            string num2 = Console.ReadLine();
+            
+            Console.WriteLine(num +" multiplied by itself is " + mathOperation.SquareNum(num));
+            Console.WriteLine("In Hawai'i, you would pay $" + mathOperation.DecimalNum(num) + " more in taxes than you would in Oregon for the same price.");
+            Console.WriteLine(num2 + " multiplied by 1000 is " + mathOperation.StringNum(num2));
+            Console.ReadLine();
+        }
+    }
+}
