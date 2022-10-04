a050f69bd3417e4f962b41c573a8515905f104c1
Hanh Le <jamiehanhle@gmail.com>
2022-10-04 15:10:34 -0700

    Completed MethodAssignment, which works with multiple parameters, but also one parameter is optional

---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Methods
+    {
+        public int TwoIntegerMethod(int num1, int optionalNum = 1)
+        {
+            int TwoInt = ((num1 * 10) / optionalNum);
+            return TwoInt ;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Methods methodAssignment = new Methods();
+
+            
+
+            Console.WriteLine("Please enter up to two numbers to perform a math operation on them.");
+            Console.WriteLine("Enter your first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number (optional):");
+
+            //try/catch statement to catch optionalNum if user does not put input in
+            try
+            {
+                //we need this because setting an optionalNum will make it into a string. Easier to make try/catch to try when 
+                // there is an optional second number inputted
+                int optionalNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(methodAssignment.TwoIntegerMethod(num1, optionalNum));
+            }
+
+            //otherwise the catch would just use the first inputted parameter and use the default for the optional parameter
+            catch
+            {
+                Console.WriteLine(methodAssignment.TwoIntegerMethod(num1));
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
