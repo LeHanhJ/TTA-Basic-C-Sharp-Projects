9aff8c822b9745f2ac83e306b1ffcffae00b894e
Hanh Le <jamiehanhle@gmail.com>
2022-10-04 19:31:54 -0700

    Worked on MethodClass Assignment and added comments

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Methods
+    {   
+        //void method that takes two integers as parameters.
+        public void sum(int num1, int num2)
+        {
+            //Have a math operation on the first integer
+            num1 = num1 + 50;
+            //Display the second integer to the screen
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //instantiate the class
+            Methods practiceMethods = new Methods();
+
+            //passing in two numbers
+            practiceMethods.sum(2,5);
+            //specifying parameters by name
+            practiceMethods.sum(num1: 9, num2: 6);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
