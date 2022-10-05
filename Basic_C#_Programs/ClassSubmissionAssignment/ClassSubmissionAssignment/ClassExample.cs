a7a3389445b473a4563f5469666d4b5d4d877c83
Hanh Le <jamiehanhle@gmail.com>
2022-10-05 14:15:16 -0700

    Uploaded ClassSubmissionAssignment

---
+++ Basic_C#_Programs/ClassSubmissionAssignment/ClassSubmissionAssignment/ClassExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class ClassExample
+    {
+        public void outputInt(int num1)
+        {
+            int quotient = num1 / 2;
+            Console.WriteLine(quotient);
+        }
+
+        //output parameters
+        public static void outputParameters(out int numDivided)
+        {
+            int dividedByTwo = 2;
+            numDivided = dividedByTwo;
+        }
+
+        //overloaded method and declared to be static
+        public static void outputInt(string num1, int num = 2)
+        {
+            int RealNum = Convert.ToInt32(num1);
+            int NumQuotient = RealNum / num;
+            Console.WriteLine(NumQuotient);
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            ClassExample operations = new ClassExample();
+
+
+            Console.WriteLine("Please enter a number and we will divide that number by 2.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // call method on the inputted number and displays output to screen
+            operations.outputInt(num1);
+
+
+            outputParameters(out num1);
+
+            Console.WriteLine("After method call, value of num 1 : {0}", num1);
+            Console.ReadLine();
+
+        }
+    }
+}
