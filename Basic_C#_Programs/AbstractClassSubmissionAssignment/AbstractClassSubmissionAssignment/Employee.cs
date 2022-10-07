54bf2b608632c447451e77e3e922c3b6483ddedd
Hanh Le <jamiehanhle@gmail.com>
2022-10-07 14:54:01 -0700

    Updated with IQuittable interface and new IQuittable object

--- Basic_C#_Programs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
+++ Basic_C#_Programs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace AbstractClassSubmissionAssignment
 {
-    class Employee : Person
+    class Employee : Person, IQuittable // class Employee inherits interface
     {
         //uses override so we are about to have method SayName() in abstract class Person run
         public override void SayName()
         {
             Console.WriteLine("Hello, my name is " + firstName + " " + lastName);
+        }
+
+        //implements Quit() method
+        public void Quit()
+        {
+            Console.WriteLine("I am quitting. Goodbye.");
         }
     }
 }
--- Basic_C#_Programs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Program.cs
+++ Basic_C#_Programs/AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Program.cs
@@ -10,11 +10,18 @@ namespace AbstractClassSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            //Created an object of type IQuittable
+            IQuittable quitter = new Employee();
+
             Employee employee = new Employee();
             employee.firstName = "Sample";
             employee.lastName = "Student";
 
             employee.SayName();
+
+            //calling the Quit() method on the new object
+            quitter.Quit();
+
             Console.ReadLine();
 
         }
