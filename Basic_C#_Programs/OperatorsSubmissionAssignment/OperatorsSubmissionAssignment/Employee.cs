8504c22994344e7d9cf06dc54bd773c7e1a32f31
Hanh Le <jamiehanhle@gmail.com>
2022-10-11 19:31:53 -0700

    Added comments to code

--- Basic_C#_Programs/OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Employee.cs
+++ Basic_C#_Programs/OperatorsSubmissionAssignment/OperatorsSubmissionAssignment/Employee.cs
@@ -13,16 +13,21 @@ namespace OperatorsSubmissionAssignment
         public string LastName { get; set; }
 
         // *Overloading the operators == and !=* //
+
+        //sets the == operator as a boolean and defined parameters
         public static bool operator== (Employee emp1, Employee emp2)
         {
+            //states that if the Id of emp 1 is equal to emp 2, then return true; otherwise return false
             if (emp1.Id.Equals(emp2.Id))
             {
                 return true;
             }
             return false;
         }
+
         public static bool operator!= (Employee emp1, Employee emp2)
         {
+            //as above, but the opposite
             if (!emp1.Id.Equals(emp2.Id))
             {
                 return true;
